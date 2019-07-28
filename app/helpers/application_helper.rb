f0377ef807367ff719c295c52f25db65c502df19
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-07-28 20:57:35 +0530

    Styled user edit view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-	def avatar_url(user, size)	   
-	   if user.image
-	   	 "http://graph.facebook.com/#{user.uid}/picture?type=large" 
-	   else 
-		 gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-     	 url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-	   end
-	end
+  def avatar_url(user)
+    if user.image
+      "https://graph.facebook.com/#{user.uid}/picture?type=large"
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=150"
+    end
+  end
 end
